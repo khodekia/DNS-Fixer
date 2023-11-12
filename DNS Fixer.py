011bf80f85f84d970d40deb0024dc89619f39149
Heis5ecure <148627666+Heis5ecure@users.noreply.github.com>
2023-11-12 19:45:20 +0330

    Update DNS Fixer.py
    adding comments

--- DNS Fixer.py
+++ DNS Fixer.py
@@ -1,6 +1,6 @@
 import requests
 import os
-
+#at the moment this code only checks if it is running with root privileges and checks your internet connection
 url = "https://www.google.com/"
 timeout = 5
 
@@ -10,6 +10,7 @@ if os.geteuid() == 0:
         request = requests.get(url, timeout=timeout)
         print(request)
         print("Connected to the Internet")
+        #this code is not completed it will be updated with a working cod
     except (requests.ConnectionError, requests.Timeout) as exception:
         print("No internet connection.")
 
